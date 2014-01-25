a4eff31ba47441b4aa9d174e47f4acb2a3922d45
H.J. Lu <hjl.tools@gmail.com>
2014-01-25 08:00:23 -0800

    Update x86_64/bits/wordsize.h from glibc 2.18
    Need it fro x32 support

--- libc/sysdeps/linux/x86_64/bits/wordsize.h
+++ libc/sysdeps/linux/x86_64/bits/wordsize.h
@@ -1,9 +1,13 @@
 /* Determine the wordsize from the preprocessor defines.  */
 
-#if defined __x86_64__
+#if defined __x86_64__ && !defined __ILP32__
 # define __WORDSIZE	64
-/* This makes /var/run/utmp compatible with 32-bit environment: */
-# define __WORDSIZE_COMPAT32	1
 #else
 # define __WORDSIZE	32
+#endif
+
+#ifdef __x86_64__
+# define __WORDSIZE_TIME64_COMPAT32	1
+/* Both x86-64 and x32 use the 64-bit system call interface.  */
+# define __SYSCALL_WORDSIZE		64
 #endif
